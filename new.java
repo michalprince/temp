b0693c3ab796100ebebd275bfe11aeec06e2df0e
michal.nankensky@wdc.com <michal.nankensky@wdc.com>
2023-04-17 15:16:04 +0300

    Test

---
+++ new.java
@@ -0,0 +1,18 @@
+package bhupen.in.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+		System.Out.Println("Welcome");
+    }
+	
+	public String getName(String name){
+		return "My name is ghghg yuio ghg" + name;
+	
+
+}
